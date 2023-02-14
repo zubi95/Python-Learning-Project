b1077f7d4710863ee12cff81aa9a1690fbbe717a
Md Zubaer Rahman <mdzubaerrahman@gmail.com>
2023-02-14 23:31:32 +1300

    initial commit

---
+++ boolean_variable.py
@@ -0,0 +1,16 @@
+# I check to see if the requirements for the honour roll are met
+
+gpa = float(input('What was your Grade Point Average? '))
+lowest_grade = float(input('What was your lowest grade'))
+
+if gpa >= .85 and lowest_grade >= .70:
+    honour_roll = True
+else:
+    honour_roll = False
+
+# Somewhare later in your code if you need to check if students is
+# on honour roll, all I need to do is check the boolean variable
+# I set earlier in my code
+if honour_roll:
+    print('You made honour roll')
+
